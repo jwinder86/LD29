50ad1bbf9cbaf1dd66a72c950e3815347f54d3c5
John Winder <jwinder86@gmail.com>
2014-04-26 14:03:21 -0700

    Terrain

---
+++ Assets/Scripts/RandomColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomColor : MonoBehaviour {
+
+	public Color[] altColors;
+
+	// Use this for initialization
+	void Start () {
+		int index = Random.Range(0, altColors.Length);
+
+		if (index < altColors.Length) {
+			renderer.material.color = altColors[index];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
