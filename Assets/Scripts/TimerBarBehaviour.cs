d4cef14d342496a948ea2df7e340d2c633a31a1d
sandaz13 <mike@sanders-tech.com>
2014-04-26 12:35:05 -0500

    water level

---
+++ Assets/Scripts/TimerBarBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerBarBehaviour : MonoBehaviour {
+	
+	public Transform bar;
+	//public TextMesh text;
+	
+	public Color fullColor;
+	public Color emptyColor;
+	
+
+	
+	private Vector3 parentInitialScale;
+	private Vector3 initialScale;
+	private Vector3 initialPosition;
+	
+	// Use this for initialization
+	void Start () {
+		parentInitialScale = transform.localScale;
+		initialScale = bar.localScale;
+		initialPosition = bar.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//	float scaleMult = Mathf.Sin(Time.timeSinceLevelLoad * Mathf.PI ) + 1f;
+		//	transform.localScale = parentInitialScale * scaleMult;
+	}
+	
+	public void setStatus(float fraction, float value) {
+		Debug.Log("fraction:value:" + fraction + " : " + value);
+		bar.transform.localScale = new Vector3(initialScale.x, initialScale.y * fraction, initialScale.z);
+		bar.transform.localPosition = new Vector3(initialPosition.x, initialPosition.y + (1f + fraction) * initialScale.y / 2f, initialPosition.z);
+		bar.renderer.material.color = Color.Lerp(emptyColor, fullColor, fraction);
+		bar.renderer.enabled = fraction > 0f;
+		//text.text = value.ToString("F2");
+		//text.color = Color.Lerp(emptyColor, fullColor, fraction);
+	}
+	
+
+}
---
+++ Assets/Scripts/WaterClock.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterClock : MonoBehaviour {
+
+	private static float[] tickTimes = {5f, 4f, 3f, 2.5f, 2f, 1.5f, 1f, 0.75f, 0.5f, 0.25f, -100f};
+	
+	public float waterMax = 10f;
+	public TimerBarBehaviour display;
+	
+	private float waterLevel;
+	
+	private PigBehaviour pig;
+	private bool gameRunning;
+	
+	public string[] levelList;
+	
+	public AudioClip moreTime;
+	public AudioClip tickSound;
+	
+	private int tickIndex;
+	
+	//FadeBehaviour fade;
+	
+	
+	// Use this for initialization
+	void Start () {
+		waterLevel = 0;
+		gameRunning = true;
+		
+		pig = (PigBehaviour) FindObjectOfType(typeof(PigBehaviour));
+		
+		tickIndex = 0;
+//		fade = this.GetComponent<FadeBehaviour>();
+//		fade.FadeIn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log("gamerunning: " + gameRunning + "| waterLevel: " + waterLevel + "waterMax" + waterMax);
+		if (gameRunning) {
+			if(waterLevel >= 0f){
+				waterLevel = waterLevel + Time.deltaTime;
+
+				
+			} else {
+				waterLevel = waterMax;
+				//GameOver();
+			}
+			
+			// don't allow the boredom clock to be higher than boredomMax
+			if(waterLevel > waterMax){
+				waterLevel = waterMax;
+			}
+		} else {
+			if (waterLevel <= 0f) {
+
+			}
+		}
+		
+		//playTick();
+		
+		if(Input.GetKeyDown ("r")){
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		display.setStatus(waterLevel / waterMax, waterLevel);
+	}
+	
+	
+	public void increaseClock(float amount){
+		if (gameRunning) {
+			waterLevel -= amount;
+			//display.setShakeTime(0.5f);
+			
+			//resetTick();
+			
+			audio.PlayOneShot(moreTime);
+			
+			Debug.Log ("decreasing water: " + amount + " = " + waterLevel);
+		}
+	}
+	
+	
+	public void GameOver() {
+//		if (gameRunning) {
+//			waterLevel = 0f;
+//			pig.Die();
+//			
+//			StartCoroutine(ReloadLevel());
+//			
+//			gameRunning = false;
+//		}
+	}
+	
+	public bool isGameRunning(){
+		return gameRunning;
+	}
+	
+	
+	public void WinGame() {
+		if (gameRunning) {
+			gameRunning = false;
+			
+			//StartCoroutine(loadNextLevel());
+		}		
+	}
+	
+	
+//	private IEnumerator loadNextLevel() {
+//		
+//		yield return new WaitForSeconds(3f);
+//		fade = this.GetComponent<FadeBehaviour>();
+//		fade.FadeOut();
+//		yield return new WaitForSeconds(2f);
+//		
+//		string nextLevel = levelList[0];
+//		for (int i = 0; i < levelList.Length - 1; i++) {
+//			if (levelList[i].Equals(Application.loadedLevelName)) {
+//				nextLevel = levelList[i+1];
+//			}
+//		}
+//		
+//		Application.LoadLevel(nextLevel);
+//	}
+	
+	private IEnumerator ReloadLevel() {
+		yield return new WaitForSeconds(5f);
+		Application.LoadLevel(Application.loadedLevel);
+	}
+	
+//	private void playTick() {
+//		if (waterLevel < tickTimes[tickIndex]) {
+//			audio.PlayOneShot(tickSound);
+//			tickIndex++;
+//		}
+//	}
+	
+//	private void resetTick() {
+//		for (int i = 0; i < tickTimes.Length; i++) {
+//			if (tickTimes[i] < waterLevel) {
+//				tickIndex = i;
+//				break;
+//			}
+//		}
+//		
+//		Debug.Log("New Tick Time" + tickIndex);
+//	}
+}
