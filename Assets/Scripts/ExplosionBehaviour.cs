8bfb2462d1fca8345607807437ae59ae66c3bb93
John Winder <jwinder86@gmail.com>
2014-04-26 15:50:51 -0700

    rocket added

---
+++ Assets/Scripts/ExplosionBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(ParticleSystem))]
+[RequireComponent (typeof(AudioSource))]
+[RequireComponent (typeof(Collider))]
+public class ExplosionBehaviour : MonoBehaviour {
+	
+	private static CameraBehaviour screen = null;
+	
+	public AudioClip explosionSound;
+	
+	public float explosionForce;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void Explode() {
+		StartCoroutine(ExplodeAction());
+		
+		if (screen == null) {
+			screen = (CameraBehaviour) FindObjectOfType(typeof(CameraBehaviour));
+		}
+		
+		//screen.ShakeTime(0.5f);
+	}
+	
+	private IEnumerator ExplodeAction() {
+		particleSystem.Play();
+		audio.PlayOneShot(explosionSound);
+		
+		yield return new WaitForSeconds(0.5f);
+		
+		collider.enabled = false;
+		
+		yield return new WaitForSeconds(5f);
+		
+		Destroy(gameObject);
+	}
+	
+	public void OnTriggerEnter(Collider other) {
+		/*PigBehaviour pig = other.GetComponent<PigBehaviour>();
+		TargetTrigger target = other.GetComponent<TargetTrigger>();
+		
+		if (pig != null) {
+			pig.Stun();
+		}
+		
+		if (target != null){
+			target.Explode();
+		}*/
+		
+		if (other.rigidbody != null) {
+			Vector3 direction = (other.transform.position - transform.position);
+			direction.z = 0f;
+			direction = direction.normalized;
+			
+			other.rigidbody.AddForce(direction * explosionForce, ForceMode.VelocityChange);
+		}
+	}
+}
---
+++ Assets/Scripts/RocketBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+[RequireComponent (typeof(Collider))]
+[RequireComponent (typeof(AudioSource))]
+public class RocketBehaviour : MonoBehaviour {
+	
+	public float rocketLifetime;
+	public float acceleration;
+	public float maxSpeed = 50;
+	
+	public ParticleSystem particleSystem;
+	public Transform model;
+	
+	public ExplosionBehaviour explosionPrefab;
+	
+	public AudioClip thrustSound;
+	
+	private bool exploded;
+	
+	// Use this for initialization
+	void Start () {
+		exploded = false;
+		
+		audio.PlayOneShot(thrustSound);
+		
+		StartCoroutine(LifetimeAction());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// thrust forward
+		Vector3 direction = transform.right;
+		rigidbody.AddForce(direction * acceleration, ForceMode.Acceleration);
+		
+		// speed limiter
+		float speed = rigidbody.velocity.magnitude;
+		if (speed > maxSpeed && !rigidbody.isKinematic){
+			rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+		}
+	}
+	
+	public void OnCollisionEnter(Collision collision) {
+		Explode();
+	}
+	
+	private void Explode() {
+		if (!exploded) {
+			exploded = true;
+			
+			ExplosionBehaviour explosion = (ExplosionBehaviour) Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+			explosion.Explode();
+			
+			// disable
+			particleSystem.Stop();
+			collider.enabled = false;
+			rigidbody.isKinematic = true;
+			Destroy(model.gameObject);
+			audio.Stop();
+		}
+		
+		// destroy later
+		StartCoroutine(DestroyAction());
+	}
+	
+	private IEnumerator LifetimeAction() {
+		yield return new WaitForSeconds(rocketLifetime);
+		Explode ();
+	}
+	
+	private IEnumerator DestroyAction() {
+		yield return new WaitForSeconds(4f);
+		Destroy(gameObject);
+	}
+}
