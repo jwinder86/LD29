73fcbef80652e361637f041710acfded230e6f90
John Winder <jwinder86@gmail.com>
2014-04-26 18:15:33 -0700

    Basic enemies

---
+++ Assets/Scripts/MineBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider))]
+public class MineBehaviour : MonoBehaviour {
+
+	public ExplosionBehaviour explosionPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		ExplosionBehaviour explosion = (ExplosionBehaviour) Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		explosion.Explode();
+		Destroy(gameObject);
+	}
+}
