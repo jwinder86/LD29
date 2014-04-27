e8b2d08dcf97c27bddbd23c91c257b7824ed4baf
sandaz13 <mike@sanders-tech.com>
2014-04-27 16:01:15 -0500

    temporary damage indicator in loot indicator

--- Assets/Scripts/LootCounterBehaviour.cs
+++ Assets/Scripts/LootCounterBehaviour.cs
@@ -5,16 +5,20 @@ public class LootCounterBehaviour : MonoBehaviour {
 
 	public TextMesh text;
 
+	public WaterClock waterClock;
+	private float damage;
+	private float pumpRate;
 	private int score;
 
 	// Use this for initialization
 	void Start () {
 		text.text = "$ 0";
+		waterClock = (WaterClock) FindObjectOfType(typeof(WaterClock));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		text.text = getLootString();
 	}
 
 	public void addLoot(int lootValue) {
@@ -23,6 +27,8 @@ public class LootCounterBehaviour : MonoBehaviour {
 	}
 
 	private string getLootString() {
-		return "$ " + score.ToString("#,##0");
+		damage=waterClock.getLeakMultiplyer();
+		pumpRate=waterClock.getPumpRate();
+		return "Damage:" + damage + " / " + pumpRate + "  -  $ " + score.ToString("#,##0");
 	}
 }
