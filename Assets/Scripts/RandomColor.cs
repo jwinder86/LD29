13dd169a4de0da5502df6eb5b4357425572293f5
John Winder <jwinder86@gmail.com>
2014-04-26 19:30:43 -0700

    stations

--- Assets/Scripts/RandomColor.cs
+++ Assets/Scripts/RandomColor.cs
@@ -4,18 +4,36 @@ using System.Collections;
 public class RandomColor : MonoBehaviour {
 
 	public Color[] altColors;
+	public bool update = false;
+	private int index = 0;
 
 	// Use this for initialization
 	void Start () {
-		int index = Random.Range(0, altColors.Length);
+		setRandomColor();
 
-		if (index < altColors.Length) {
-			renderer.material.color = altColors[index];
+		if (update) {
+			StartCoroutine(UpdateColor());
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+	}
+
+	private void setRandomColor() {
+		index += Random.Range(0, altColors.Length - 1) % altColors.Length;
+		
+		if (index < altColors.Length) {
+			renderer.material.color = altColors[index];
+		}
+	}
+
+	private IEnumerator UpdateColor() {
+		while (true) {
+			setRandomColor();
+
+			yield return new WaitForSeconds(Random.Range(1f, 8f));
+		}
 	}
 }
