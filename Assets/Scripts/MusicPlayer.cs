29c711abd292017be84d042558778148d3921b7f
sandaz13 <mike@sanders-tech.com>
2014-04-27 19:03:27 -0500

    syncing mike

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+	
+	public AudioClip[] songs;
+	
+	// Use this for initialization
+	void Start () {
+		PlayRandomSong();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!audio.isPlaying) {
+			PlayRandomSong();
+		}
+	}
+	
+	private void PlayRandomSong() {
+		AudioClip song = songs[Random.Range(0, songs.Length - 1)];
+		
+		audio.clip = song;
+		audio.Play();
+	}
+}
