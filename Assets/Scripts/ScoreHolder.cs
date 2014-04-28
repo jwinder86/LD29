38f29528ecc860762e255217371d0b2be4c32cb5
John Winder <jwinder86@gmail.com>
2014-04-28 16:23:31 -0700

    static score holder

---
+++ Assets/Scripts/ScoreHolder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreHolder {
+
+	private static int score;
+	private static ExitState exitState;
+
+	public enum ExitState {Won, Lost, Quit};
+
+	public static void exitQuit() {
+		score = 0;
+		exitState = ExitState.Quit;
+	}
+
+	public static void exitWin(int newScore) {
+		score = newScore;
+		exitState = ExitState.Won;
+	}
+
+	public static void exitLose(int newScore) {
+		score = newScore;
+		exitState = ExitState.Lost;
+	}
+
+	public static int getScore() {
+		return score;
+	}
+
+	public static ExitState getExitState() {
+		return exitState;
+	}
+}
