32115c4254bf3ed2c927d79364994242452f7606
John Winder <jwinder86@gmail.com>
2014-04-28 15:02:25 -0700

    fixed instructions, added tentacle model

---
+++ Assets/Scripts/TentacleBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float flapAngle;
+	public float flapSpeed;
+
+	private float start;
+
+	// Use this for initialization
+	void Start () {
+		start = Random.Range(0f, Mathf.PI * 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler(0f, 0f, getRot(start));
+		start += Time.deltaTime * flapSpeed;
+	}
+
+	private float getRot(float timer) {
+		return flapAngle * Mathf.Sin(timer * flapSpeed);
+	}
+}
